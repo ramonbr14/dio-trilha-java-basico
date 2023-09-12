b4fb60af79001d4660efa1a771d9482ceec6ed68
ramonbr14 <ramonbr14@gmail.com>
2023-09-12 00:29:04 -0400

    Concluida a Etapa de Tipos e Variavies lv65

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        //Tipos e Variaveis - 3
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        //Tipos e Variaveis - 4
+        int numero = 5;
+
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; 
+
+        String nome = "Ramon Silva";
+
     }
 }
